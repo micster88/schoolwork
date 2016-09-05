66fd98ef028e280b3cb25008cb21089f89cc0314
Mike Kuczkuda <mkuczkud@unca.edu>
2016-09-05 12:33:45 -0400

    no message

--- workspace/chartBuilder/src/chartBuilder/ChartBuilder.java
+++ workspace/chartBuilder/src/chartBuilder/ChartBuilder.java
@@ -2,27 +2,44 @@ package chartBuilder;
 
 public class ChartBuilder {
 	private class Square {
-		private int x;
-		private int y;
-		private int lt;
-		private int ht;
-		private Square(int x, int y, int lt, int ht){
-			this.x=x;
-			this.y=y;
-			this.lt=lt;
-			this.ht=ht;
+		private int x1;
+		private int y1;
+		private int x2;
+		private int y2;
+		private Square(int x1, int y1, int x2, int y2){
+			this.x1=x1;
+			this.y1=y1;
+			this.x2=x2;
+			this.y2=y2;
 			drawSquare();
 		}
 		private void drawSquare(){
 			
 		}
-		private void eraseSquare(){
+		public void eraseSquare(){
 			
 		}
 	}
 	private class Line {
-		
+		private int x1;
+		private int y1;
+		private int x2;
+		private int y2;
+		private Line(int x1, int y1, int x2, int y2){
+			this.x1=x1;
+			this.y1=y1;
+			this.x2=x2;
+			this.y2=y2;
+			drawLine();
+		}
+		private void drawLine(){
+			
+		}
+		public void eraseLine(){
+			
+		}
 	}
+	
 	public static void main(String[] args) {
 		
 
