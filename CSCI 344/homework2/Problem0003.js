ddd4b84ef6dc34ff495366103d88ed979f9d7ff5
Mike Kuczkuda <mkuczkud@unca.edu>
2017-02-08 15:27:00 -0500

    Project 2

--- CSCI 344/homework2/Problem0003.js
+++ CSCI 344/homework2/Problem0003.js
@@ -1,18 +1,25 @@
 //Michael Kuczkuda
 //Finds the number of odd and even numbers in the first 100 digits of multiplies of 3
-var even, odd, temp;
+var even, odd, temp, test, stringNum;
 even=0;
 odd=0;
+test=0;
 //0-99 or first hundred numbers
 for(var i=0; i<100; i++){
     //That multiple of three
     temp=3*i;
-    //Even Check or it is odd
-    if((temp%2)===0){
-        even++;
-    }
-    else{
-        odd++;
+    //makes a string
+    stringNum=temp.toString();
+    //Tests each number in the string
+    for(var j=0; j<stringNum.length; j++){
+        //gets that digit and tests if its odd or even
+        test=stringNum.charAt(j);
+        if((test%2)===0){
+            even++;
+        }
+        else{
+            odd++;
+        }
     }
 }
 console.log("Answer #3");
